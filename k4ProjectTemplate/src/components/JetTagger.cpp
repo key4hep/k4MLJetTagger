db2ee5f0615d5b4b3109bc1fdea649a12854fab2
Sara Aumiller <sara.aumiller@cern.ch>
2024-12-03 15:30:10 +0100

    retrieve IP and rename cov values to be consistent

--- k4ProjectTemplate/src/components/JetTagger.cpp
+++ k4ProjectTemplate/src/components/JetTagger.cpp
@@ -17,7 +17,7 @@
  * limitations under the License.
  */
 
-// #include "JetTagger.h"
+// #include "JetTagger.h" 
 
 // #include <vector>
 
@@ -26,7 +26,7 @@
 //#include "ROOT/RVec.hxx"
 
 #include "TLorentzVector.h"
-
+#include "TVector3.h"
 
 #include "GaudiKernel/MsgStream.h"
 #include "k4FWCore/Transformer.h"
@@ -36,17 +36,19 @@
 
 #include <random>
 
-// define the edm4hep operators
-edm4hep::Vector3f operator-(const edm4hep::Vector3f& a, const edm4hep::Vector3f& b) {
-    return edm4hep::Vector3f(a.x - b.x, a.y - b.y, a.z - b.z);
-}
-
 
 // define the structures for jet constituents (pfcand) and jets
 
 struct Pfcand {
   /**
   Structure to store the observables of a particle / jet constituent. These observables will be used as input features to the neural network for jet flavor tagging with the Particle Transformer. For CLD, these are 33 observables including kinematics, PID and track parameters.
+  * NOTE: regarding the cov matrix, we use following convenstion compared to FCCAnalysis: 
+  * HERE | FCCAnalysis | explanation
+  * d0  | xy or dxy | transverse impact parameter
+  * phi | dphi| azimuthal angle 
+  * tanLambda | ctngtheta or deta or dlambda | lambda is the dip angle of the track in r-z
+  * c | dpt | signed curvature of the track.[c] = 1/s. To convert to key4hep convention, multiply by 2/c [1/m]. 
+  * z0 | dz | longitudinal impact parameter
   */
 
   // kinematics
@@ -58,12 +60,13 @@ struct Pfcand {
 
   // track params
   // cov matrix
-  float pfcand_dptdpt, pfcand_detadeta, pfcand_dphidphi, pfcand_dxydxy, pfcand_dzdz, pfcand_dxydz, pfcand_dphidxy, pfcand_dlambdadz, pfcand_dxyc, pfcand_dxyctgtheta, pfcand_phic, pfcand_phidz, pfcand_phictgtheta, pfcand_cdz, pfcand_cctgtheta;
+  float pfcand_cov_cc, pfcand_cov_tanLambdatanLambda, pfcand_cov_phiphi, pfcand_cov_d0d0, pfcand_cov_z0z0; 
+  float pfcand_cov_d0z0, pfcand_cov_phid0, pfcand_cov_tanLambdaz0, pfcand_cov_d0c, pfcand_cov_d0tanLambda, pfcand_cov_phic, pfcand_cov_phiz0, pfcand_cov_phitanLambda, pfcand_cov_cz0, pfcand_cov_ctanLambda;
   // IP
-  float pfcand_dxy, pfcand_dz;
-  float pfcand_btagSip2dVal, pfcand_btagSip2dSig;
-  float pfcand_btagSip3dVal, pfcand_btagSip3dSig;
-  float pfcand_btagJetDistVal, pfcand_btagJetDistSig;
+  float pfcand_d0, pfcand_z0;
+  float pfcand_Sip2dVal, pfcand_Sip2dSig;
+  float pfcand_Sip3dVal, pfcand_Sip3dSig;
+  float pfcand_JetDistVal, pfcand_JetDistSig;
 };
 
 struct Jet {
@@ -83,7 +86,7 @@ struct Helix{
 };
 
 // Declare the function at the top
-int fancyModelDoTag(const edm4hep::ReconstructedParticle& jet, const edm4hep::VertexCollection& prim_vertex, MsgStream& log);
+int fancyModelDoTag(const edm4hep::ReconstructedParticle& jet, const edm4hep::VertexCollection& prim_vertex_coll, MsgStream& log);
 
 float get_relative_erel(const edm4hep::ReconstructedParticle& jet, const edm4hep::ReconstructedParticle& particle) {
     /**
@@ -134,30 +137,30 @@ Pfcand fill_track_params_neutral(Pfcand p){
   */
 
   // cov matrix
-  p.pfcand_dptdpt = -9;
-  p.pfcand_detadeta = -9;
-  p.pfcand_dphidphi = -9;
-  p.pfcand_dxydxy = -9;
-  p.pfcand_dzdz = -9;
-  p.pfcand_dxydz = -9;
-  p.pfcand_dphidxy = -9;
-  p.pfcand_dlambdadz = -9;
-  p.pfcand_dxyc = -9;
-  p.pfcand_dxyctgtheta = -9;
-  p.pfcand_phic = -9;
-  p.pfcand_phidz = -9;
-  p.pfcand_phictgtheta = -9;
-  p.pfcand_cdz = -9;
-  p.pfcand_cctgtheta = -9;
+  p.pfcand_cov_cc = -9;
+  p.pfcand_cov_tanLambdatanLambda = -9;
+  p.pfcand_cov_phiphi = -9;
+  p.pfcand_cov_d0d0 = -9;
+  p.pfcand_cov_z0z0 = -9;
+  p.pfcand_cov_d0z0 = -9;
+  p.pfcand_cov_phid0 = -9;
+  p.pfcand_cov_tanLambdaz0 = -9;
+  p.pfcand_cov_d0c = -9;
+  p.pfcand_cov_d0tanLambda = -9;
+  p.pfcand_cov_phic = -9;
+  p.pfcand_cov_phiz0 = -9;
+  p.pfcand_cov_phitanLambda = -9;
+  p.pfcand_cov_cz0 = -9;
+  p.pfcand_cov_ctanLambda = -9;
   // IP
-  p.pfcand_dxy = -9;
-  p.pfcand_dz = -9;
-  p.pfcand_btagSip2dVal = -9;
-  p.pfcand_btagSip2dSig = -200;
-  p.pfcand_btagSip3dVal = -9;
-  p.pfcand_btagSip3dSig = -200;
-  p.pfcand_btagJetDistVal = -9;
-  p.pfcand_btagJetDistSig = -200;
+  p.pfcand_d0 = -9;
+  p.pfcand_z0 = -9;
+  p.pfcand_Sip2dVal = -9;
+  p.pfcand_Sip2dSig = -200;
+  p.pfcand_Sip3dVal = -9;
+  p.pfcand_Sip3dSig = -200;
+  p.pfcand_JetDistVal = -9;
+  p.pfcand_JetDistSig = -200;
   
   return p;
 }
@@ -213,33 +216,31 @@ Pfcand fill_cov_matrix(Pfcand p, const edm4hep::ReconstructedParticle& particle)
   // get the track
   auto track = particle.getTracks()[0].getTrackStates()[0]; // get info at interaction point
   // diagonal elements
-  p.pfcand_dxydxy = track.covMatrix[0];
-  p.pfcand_dphidphi = track.covMatrix[2];
-  p.pfcand_dptdpt = track.covMatrix[5]; // omega
-  p.pfcand_dzdz = track.covMatrix[9];
-  p.pfcand_detadeta = track.covMatrix[14]; // tanLambda
+  p.pfcand_cov_d0d0 = track.covMatrix[0];
+  p.pfcand_cov_phiphi = track.covMatrix[2];
+  p.pfcand_cov_cc = track.covMatrix[5]; // omega
+  p.pfcand_cov_z0z0 = track.covMatrix[9];
+  p.pfcand_cov_tanLambdatanLambda = track.covMatrix[14]; // tanLambda
   // off-diagonal elements
-  p.pfcand_dxydz = track.covMatrix[6];
-  p.pfcand_dphidxy = track.covMatrix[1];
-  p.pfcand_dlambdadz = track.covMatrix[13];
-  p.pfcand_dxyc = track.covMatrix[3];
-  p.pfcand_dxyctgtheta = track.covMatrix[10];
-  p.pfcand_phic = track.covMatrix[4];
-  p.pfcand_phidz = track.covMatrix[7];
-  p.pfcand_phictgtheta = track.covMatrix[11];
-  p.pfcand_cdz = track.covMatrix[8];
-  p.pfcand_cctgtheta = track.covMatrix[12];
+  p.pfcand_cov_d0z0 = track.covMatrix[6];
+  p.pfcand_cov_phid0 = track.covMatrix[1];
+  p.pfcand_cov_tanLambdaz0 = track.covMatrix[13];
+  p.pfcand_cov_d0c = track.covMatrix[3];
+  p.pfcand_cov_d0tanLambda = track.covMatrix[10];
+  p.pfcand_cov_phic = track.covMatrix[4];
+  p.pfcand_cov_phiz0 = track.covMatrix[7];
+  p.pfcand_cov_phitanLambda = track.covMatrix[11];
+  p.pfcand_cov_cz0 = track.covMatrix[8];
+  p.pfcand_cov_ctanLambda = track.covMatrix[12];
 
   return p;
 }
 
-Helix calculate_helix_params(const edm4hep::ReconstructedParticle& particle, const edm4hep::VertexCollection& prim_vertex){
+const edm4hep::Vector3f get_primary_vertex(const edm4hep::VertexCollection& prim_vertex){
   /**
-  * we must extract the helix parametrisation of the track with respect to the PRIMARY VERTEX. 
-  * This is done like in: https://github.com/HEP-FCC/FCCAnalyses/blob/63d346103159c4fc88cdee7884e09b3966cfeca4/analyzers/dataframe/src/ReconstructedParticle2Track.cc#L64 
-  * @param particle: the charged particle / jet constituent which track should be parametrized
+  * Get the primary vertex of the event.
   * @param prim_vertex: the primary vertex collection of the event
-  * @return: helix object filled with track parametrization with respect to the primary vertex
+  * @return: the primary vertex
   */
 
   // get primary vertex
@@ -258,6 +259,18 @@ Helix calculate_helix_params(const edm4hep::ReconstructedParticle& particle, con
     throw std::invalid_argument("No primary vertex found");
   }
 
+  return pv_pos;
+}
+
+Helix calculate_helix_params(const edm4hep::ReconstructedParticle& particle, const edm4hep::Vector3f& pv_pos){
+  /**
+  * we must extract the helix parametrisation of the track with respect to the PRIMARY VERTEX. 
+  * This is done like in: https://github.com/HEP-FCC/FCCAnalyses/blob/63d346103159c4fc88cdee7884e09b3966cfeca4/analyzers/dataframe/src/ReconstructedParticle2Track.cc#L64 
+  * @param particle: the charged particle / jet constituent which track should be parametrized
+  * @param pv_pos: the primary vertex position of the event
+  * @return: helix object filled with track parametrization with respect to the primary vertex
+  */
+
   // get track
   auto track = particle.getTracks()[0].getTrackStates()[0]; // get info at interaction point
   // get other needed parameters
@@ -269,7 +282,7 @@ Helix calculate_helix_params(const edm4hep::ReconstructedParticle& particle, con
 
   // calculate helpers
   const edm4hep::Vector3f point_on_track = edm4hep::Vector3f(- track.D0 * std::sin(track.phi), track.D0 * std::cos(track.phi), track.Z0); // point on particle track
-  const edm4hep::Vector3f x = edm4hep::Vector3f(point_on_track.x - pv_pos.x, point_on_track.y - pv_pos.y, point_on_track.z - pv_pos.z); //point_on_track - *pv_pos; // vector from primary vertex to point on track
+  const edm4hep::Vector3f x = edm4hep::Vector3f(point_on_track.x - pv_pos.x, point_on_track.y - pv_pos.y, point_on_track.z - pv_pos.z); //point_on_track - pv_pos; // vector from primary vertex to point on track
   const float pt = std::sqrt(p.x*p.x + p.y*p.y); // transverse momentum of particle
   const float a = - q * Bz * cSpeed; // Lorentz force on particle in magnetic field
   const float r2 = x.x*x.x + x.y*x.y;
@@ -316,15 +329,51 @@ Helix calculate_helix_params(const edm4hep::ReconstructedParticle& particle, con
   return h;
 }
 
-Pfcand fill_track_IP(Pfcand p, Helix h){
+Pfcand fill_track_IP(const edm4hep::ReconstructedParticle& jet, const edm4hep::ReconstructedParticle& particle, Pfcand p, Helix h, const edm4hep::Vector3f& prim_vertex){
   /**
   Calculate the impact parameters of the track with respect to the primary vertex. The helix parametrization of the particle track is with respect to the primary vertex.
+  * @param jet: the jet
+  * @param particle: the charged particle / jet constituent
   * @param p: the particle object to fill
   * @param h: the helix object with the track parametrization
   * @return: the particle object with filled impact parameters
   */
 
-  // calculate impact parameters
+  // IP
+  p.pfcand_d0 = h.d0;
+  p.pfcand_z0 = h.z0;
+
+  // signed IP
+  TVector3 jet_p(jet.getMomentum().x, jet.getMomentum().y, jet.getMomentum().z); 
+  TVector3 part_p(particle.getMomentum().x, particle.getMomentum().y, particle.getMomentum().z);
+
+  // calculate distance of closest approach in 3d - like in https://github.com/HEP-FCC/FCCAnalyses/blob/d39a711a703244ee2902f5d2191ad1e2367363ac/analyzers/dataframe/src/JetConstituentsUtils.cc#L616-L646 
+  TVector3 n = part_p.Cross(jet_p).Unit(); // direction of closest approach
+  TVector3 part_pnt(- h.d0 * std::sin(h.phi), h.d0 * std::cos(h.phi), h.z0); // point on particle track
+  TVector3 jet_pnt(prim_vertex.x, prim_vertex.y, prim_vertex.z) ; //edm4hep::Vector3f(0,0,0); // point on jet - THIS DIFFERS FROM THE FCCANALYSIS IMPLEMENTATION
+  const float d_3d = n.Dot(part_pnt - jet_pnt); // distance of closest approach
+  p.pfcand_JetDistVal = d_3d;
+
+  // calculate signed 2D impact parameter - like in https://github.com/HEP-FCC/FCCAnalyses/blob/d39a711a703244ee2902f5d2191ad1e2367363ac/analyzers/dataframe/src/JetConstituentsUtils.cc#L392-L419 
+  const float sip2d = std::copysign(std::abs(h.d0), part_pnt.X() * jet_p.X() + part_pnt.Y() * jet_p.Y()); // dot product between part and jet in 2D: if angle between track and jet greater 90 deg -> negative sign; if smaller 90 deg -> positive sign
+  p.pfcand_Sip2dVal = sip2d;
+  p.pfcand_Sip2dSig = (p.pfcand_cov_d0d0 > 0) ? (sip2d / std::sqrt(p.pfcand_cov_d0d0)) : -999;
+
+  // calculate signed 3D impact parameter - like in https://github.com/HEP-FCC/FCCAnalyses/blob/d39a711a703244ee2902f5d2191ad1e2367363ac/analyzers/dataframe/src/JetConstituentsUtils.cc#L503-L531 
+  const float IP_3d = std::sqrt(h.d0*h.d0 + h.z0*h.z0);
+  const float sip3d = std::copysign(std::abs(IP_3d),  part_pnt.Dot(jet_p)); // dot product between part in jet in 3d: if angle between track and jet greater 90 deg -> negative sign; if smaller 90 deg -> positive sign
+  p.pfcand_Sip3dVal = sip3d;
+
+  // significance in 3D
+  const float in_sqrt = p.pfcand_cov_d0d0 + p.pfcand_cov_z0z0; 
+  if (in_sqrt>0){ // can caluclate sqrt?
+    const float err3d = std::sqrt(in_sqrt); // error of distance of closest approach
+    p.pfcand_JetDistSig = d_3d/err3d; 
+    p.pfcand_Sip3dSig = sip3d/err3d; 
+  } else { // handle error -> dummy value
+    p.pfcand_JetDistSig = -999;
+    p.pfcand_Sip3dSig = -999;
+  }
 
   return p;
 }
@@ -358,7 +407,7 @@ struct JetTagger
 };
 
 // Function that simulates a tagging model by returning a dummy value.
-int fancyModelDoTag(const edm4hep::ReconstructedParticle& jet, const edm4hep::VertexCollection& prim_vertex, MsgStream& log) {
+int fancyModelDoTag(const edm4hep::ReconstructedParticle& jet, const edm4hep::VertexCollection& prim_vertex_coll, MsgStream& log) {
     // Create a random number generator for demonstration purposes.
     static std::mt19937 rng(42);  // Seed for reproducibility
     static std::uniform_int_distribution<int> dist(0, 6);  // Assume 7 jet flavors: 0 to 6
@@ -368,6 +417,7 @@ int fancyModelDoTag(const edm4hep::ReconstructedParticle& jet, const edm4hep::Ve
 
     // Create a jet object
     Jet j;
+    const edm4hep::Vector3f prim_vertex = get_primary_vertex(prim_vertex_coll);
 
     // loop over all jet constituents and retrieve 33 input features to the network
     for (const auto& particle : jet.getParticles()) {
@@ -390,7 +440,7 @@ int fancyModelDoTag(const edm4hep::ReconstructedParticle& jet, const edm4hep::Ve
       if (n_tracks == 1) { // charged particle
         p = fill_cov_matrix(p, particle); // covariance matrix
         Helix h = calculate_helix_params(particle, prim_vertex); // calculate track parameters described by a helix parametrization
-        p = fill_track_IP(p, h);
+        p = fill_track_IP(jet, particle, p, h, prim_vertex); // impact parameters
       } else if (n_tracks == 0) { // neutral particle
         p = fill_track_params_neutral(p);
       } else {
