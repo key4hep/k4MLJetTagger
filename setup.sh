77427af79e931b0e60c0bf9c54d69fb4a682ec7a
Sara Aumiller <sara.aumiller@cern.ch>
2025-03-31 14:35:58 +0200

    remote torch from setup

--- setup.sh
+++ setup.sh
@@ -6,9 +6,6 @@
 source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 # source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-11-28
 
-TORCH_PATH=$(dirname $(python -c 'import torch; print(f"{torch.__file__}")'))
-export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:${TORCH_PATH}/share/cmake
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TORCH_PATH}/lib
 
 ONNXRUNTIME_PATH=$(dirname $(python -c 'import onnxruntime; print(f"{onnxruntime.__file__}")'))
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:${ONNXRUNTIME_PATH}/../../../..
