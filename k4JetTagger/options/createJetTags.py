ae3ec3137dd037534df0fa5396f451ac49cf6659
Sara Aumiller <sara.aumiller@cern.ch>
2025-03-31 14:32:11 +0200

    add input file arg parser to createJetTags.py

--- k4JetTagger/options/createJetTags.py
+++ k4JetTagger/options/createJetTags.py
@@ -22,11 +22,18 @@ from Configurables import k4DataSvc
 from Configurables import EventDataSvc
 from Configurables import CollectionMerger
 from k4FWCore import ApplicationMgr, IOSvc
+from k4FWCore.parseArgs import parser
+
+# parse the custom arguments
+parser_group = parser.add_argument_group("createJetTags.py custom options")
+parser_group.add_argument("--inputFiles", nargs="+", metavar=("file1", "file2"), help="One or multiple input files", 
+                        default=["/eos/experiment/fcc/prod/fcc/ee/test_spring2024/240gev/Hbb/CLD_o2_v05/rec/00016783/000/Hbb_rec_16783_99.root"])
+parser_group.add_argument("--outputFile", help="Output file name", default="output_jettags.root")
+args = parser.parse_known_args()[0]
 
 svc = IOSvc("IOSvc")
-#svc.Input = "/eos/experiment/fcc/prod/fcc/ee/test_spring2024/240gev/Hbb/CLD_o2_v05/rec/00016783/000/Hbb_rec_16783_1.root"
-svc.Input = "/eos/experiment/fcc/prod/fcc/ee/test_spring2024/240gev/Hbb/CLD_o2_v05/rec/00016783/000/Hbb_rec_16783_99.root"
-svc.Output = "output_jettagging.root"
+svc.Input = args.inputFiles
+svc.Output = args.outputFile
 
 #svc.outputCommands = [
 #    "drop *",
