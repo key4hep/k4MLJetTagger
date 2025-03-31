7c86523c1a2f21f556a724763485d525e113e1bf
Sara Aumiller <sara.aumiller@cern.ch>
2025-03-31 10:48:37 +0200

    use nighlies instead of fixed software stack

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Script to setup key4hep nightly environment that allows to use (py)torch, ONNX and Marlin.
 
 
-#source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
-source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-11-28
+source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
+# source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-11-28
 
 TORCH_PATH=$(dirname $(python -c 'import torch; print(f"{torch.__file__}")'))
 export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:${TORCH_PATH}/share/cmake
