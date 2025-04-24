434b16def8712c6a96ddd30e8ccf69b4be094e15
Sara Aumiller <sara.aumiller@cern.ch>
2025-04-24 13:08:19 +0200

    update submitting to condor file

--- extras/submit_to_condor/submitJob.sh
+++ extras/submit_to_condor/submitJob.sh
@@ -18,9 +18,10 @@ LOCAL=${8} # if true, use k4MLJetTagger locally, otherwise use key4hep stack
 OUTPUT=${OUTPUT_DIR}/${OUTPUT_FILE}
 
 # setup the env
-if [ "$LOCAL" = true ]; then
+if [[ "${LOCAL,,}" == "true" ]]; then
   cluster_loc=$(pwd)
-  cd $(TAGGER_DIR)
+  echo "[Info] Running k4MLJetTagger locally ... from ${TAGGER_DIR}"
+  cd ${TAGGER_DIR}
   source ./setup.sh
   k4_local_repo
   cd $cluster_loc
@@ -30,17 +31,16 @@ fi
 
 # get input files
 root_files=($(ls ${FILE_PATTERN} 2>/dev/null | sort | tail -n +$((FROM_I + 1)) | head -n ${NUM_FILES})) # works fine even if NUM_FILES is larger than the number of files available 
-echo "loading root files ..."
+echo "[Info] Loading root files ..."
 
 # Check if there are any matching files
 if [ ${#root_files[@]} -eq 0 ]; then
-    echo "No root files found matching the pattern."
+    echo "[Error] No root files found matching the pattern."
     exit 1
 fi
-
-pwd 
+ 
 # make directory
-mkdir job
+mkdir -p job
 
 # Copy each file using the Python script
 for file in "${root_files[@]}"; do
@@ -49,40 +49,45 @@ for file in "${root_files[@]}"; do
     # Copy the file using the Python script
     python3 /afs/cern.ch/work/f/fccsw/public/FCCutils/eoscopy.py "${file}" "./job/${base_name}"
 done
-echo "loaded root files ..."
+echo "[Info] Loaded root files ..."
 
 # Extract the pattern (e.g., Hbb, Huu, etc.)
 pattern=$(echo "${FILE_PATTERN}" | grep -oE '/H[[:alnum:]]+/')
 pattern=${pattern:1:-1} # Remove the leading and trailing slashes
 
-echo "pattern: ${pattern}"
+# Check if the pattern is empty
+if [ -z "${pattern}" ]; then
+    echo "[Error] Failed to extract pattern from FILE_PATTERN: ${FILE_PATTERN}"
+    exit 1
+fi
+
+echo "[Info] Found following from pattern: ${pattern}"
 
 # merge multiple root files to one input file
 hadd "./job/merged_input_${pattern}.root" ./job/*.root
 preproc_time=$(date +%s)
 middle_time=$((preproc_time - start_time))
-echo "time before running script: $middle_time seconds"
+echo "[Info] Time before running script: $middle_time seconds"
 
-echo "running script ..."
+# Run the script
+cmd="k4run \"${TAGGER_DIR}/k4MLJetTagger/options/${JOB}\" --inputFiles=\"./job/merged_input_${pattern}.root\" --outputFile=\"./job/out.root\""
+echo "[INFO] Running command: $cmd"
+eval $cmd
+echo "[Info] Job done ... "
+job_time=$(($(date +%s) - preproc_time))
+echo "[Info] Time to run job: $job_time seconds"
 
-k4run "${TAGGER_DIR}/k4MLJetTagger/options/${JOB}" --inputFiles="./job/merged_input_${pattern}.root" --outputFile="./job/out.root" 
-
-echo "job done ... "
-job_endtime=$(date +%s)
-job_time=$((job_endtime - preproc_time)) 
-echo "time to run job: $job_time seconds"
 # make directory if it does not exist:
-if [ ! -d ${OUTPUT_DIR} ]; then
-  mkdir -p ${OUTPUT_DIR}
+if [ ! -d "${OUTPUT_DIR}" ]; then
+  mkdir -p "${OUTPUT_DIR}"
 fi
 
-
-echo "copying output file to: ${OUTPUT}"
-
 # copy file to output dir
-python /afs/cern.ch/work/f/fccsw/public/FCCutils/eoscopy.py ./job/out.root ${OUTPUT}
-echo "Ran script successfully!"
-end_time=$(date +%s)
-execution_time=$((end_time - start_time))
-echo "Execution time: $execution_time seconds"
+echo "[Info] Copying output file to: ${OUTPUT}"
+python3 /afs/cern.ch/work/f/fccsw/public/FCCutils/eoscopy.py ./job/out.root ${OUTPUT}
+
+# Finish info
+echo "[Info] Ran script successfully!"
+execution_time=$(($(date +%s) - start_time))
+echo "[Info] Execution time: $execution_time seconds"
 
--- extras/submit_to_condor/write_sub.py
+++ extras/submit_to_condor/write_sub.py
@@ -3,6 +3,9 @@
 # data you want to process
 input_data_path = "/eos/experiment/fcc/prod/fcc/ee/test_spring2024/240gev/{pattern}/CLD_o2_v05/rec/{prod}/*/{pattern}_rec_*.root"
 data_pattern = {"Huu": "00016774", "Hdd": "00016771", "Hss": "00016777", "Hcc": "00016780", "Hbb": "00016783", "Hgg": "00016768", "Htautau": "00016765"} # the key is the "pattern" and the value is the "prod" number
+ 
+num_files = 5 # How many files to process per job
+start_indices = range(0, 100, num_files) # total of 100 files per flavor with 1000 events each
 
 # output paths
 output_base = "/eos/experiment/fcc/ee/datasets/CLD_fullsim_tagging_key4hep_dummy_folder/" 
@@ -13,16 +16,13 @@ job = "writeJetConstObs.py" # writeJetTags.py # which steering file to use
 local_path_to_tagger = "/afs/cern.ch/work/s/saaumill/public/k4MLJetTagger/" # CHANGE your path here (used for path to your steering file (job) and in case of local==True, the k4MLJetTagger is locally used and NOT from the stack)
 
 # if you want to run k4MLJetTagger locally or from the latest stack
-local = False
-
-### OPTIONAL TO CHANGE
-num_files = 5 # How many files to process per job
-start_indices = range(0, 100, num_files) # total of 100 files per flavor with 1000 events each
+local = True
 
 # condor settings
 acounting_group = "group_u_FCC.local_gen" 
 job_flavour = "longlunch" # check out options here: https://batchdocs.web.cern.ch/local/submit.html
 
+### DO NOT CHANGE ANYTHING BELOW THIS LINE
 
 def generate_sub_file():
     """
@@ -54,9 +54,7 @@ log                   = {output_log}job.$(ClusterId).$(ClusterId).log
         for start_index in start_indices:
             input_pattern = input_data_path.format(pattern=pattern, prod=data_pattern[pattern])
             output_file = f"{pattern}_{job_counter}.root"
-            arguments = f"{job} {start_index} {num_files} \'{input_pattern}\' {output_base} {output_file} {local}"
-            if local:
-                arguments += f" {local_path_to_tagger}"
+            arguments = f"{job} {start_index} {num_files} \'{input_pattern}\' {output_base} {output_file} {local_path_to_tagger} {local}"
             content += f"arguments=\"{arguments}\"\nqueue\n"
             job_counter += 1
 
